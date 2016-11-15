bed26e0450b3ee16de526975d32b8b5ba801644a
Casiano Rodriguez Leon <casiano.rodriguez.leon@gmail.com>
2016-11-15 16:35:14 +0000

    plural

--- server.js
+++ server.js
@@ -1,19 +1,20 @@
 var express = require('express'),
     app = express(),
-    sqlite3 = require('sqlite3').verbose(),
+    Sqlite3 = require('sqlite3').verbose(),
     bodyParser = require('body-parser'),
-    db = new sqlite3.Database('cozy');
+    db = new Sqlite3.Database('bookmarks.sqlite');
 
 /* We add configure directive to tell express to use Pug to
    render templates */
 app.set('views', __dirname + '/views');
-app.engine('pug', require('pug').__express);
+// app.set('view engine', 'pug');
+// See http://expressjs.com/en/api.html#app.engine
+app.engine('pug', require('pug').__express); 
 
 // Allows express to get data from POST requests
 app.use(bodyParser.urlencoded({
     extended: true
 }));
-app.use(bodyParser.json());
 
 // Database initialization
 db.get("SELECT name FROM sqlite_master WHERE type='table' AND name='bookmarks'", function(err, row) {
@@ -40,12 +41,12 @@ db.on('trace', (m)=> console.log(`Running query: ${m}`));
 // We render the templates with the data
 app.get('/', function(req, res) {
 
-    db.all('SELECT * FROM bookmarks ORDER BY title', function(err, row) {
+    db.all('SELECT * FROM bookmarks ORDER BY title', function(err, rows) {
         if(err !== null) {
             res.status(500).send("An error has occurred -- " + err);
         }
         else {
-            res.render('index.pug', {bookmarks: row}, function(err, html) {
+            res.render('index.pug', {bookmarks: rows}, function(err, html) {
                 res.status(200).send(html);
             });
         }
